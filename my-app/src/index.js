a80cc4d15363cd44c99fa85251f768fbe696e465
guanshibin <guanshibin@qing5.com>
2018-06-14 11:06:59 +0800

    +react 命令行创建 React 应用及开发生产环境

---
+++ my-app/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
