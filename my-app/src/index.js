5bcdfcb89d3ffff6e08d5dba2fd82f95cb2fd842
guanshibin <guanshibin@qing5.com>
2018-07-05 15:56:27 +0800

    *React 条件渲染

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -153,3 +153,25 @@ ReactDOM.render(
     <Toggle />,
     document.getElementById('toggle')
 );
+
+//React 条件渲染
+function UserGreeting(props) {
+    return <h1>欢迎回来!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>请先登录.</h1>;
+}
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+ReactDOM.render(
+    // Try changing to isLoggedIn={true}:
+    <Greeting isLoggedIn={false} />,
+    document.getElementById('greet')
+);
