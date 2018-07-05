3a0c6d2f2f0e9e7e8481c6c1bd98e2d61184eb38
guanshibin <guanshibin@qing5.com>
2018-07-05 14:00:40 +0800

    *体验用ES6类的方式创建组件，注意 与 函数形式创建组件 的区别

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -64,3 +64,25 @@ ReactDOM.render(
     <InClass />,
     document.getElementById('xiongChuMo')
 );
+
+//用ES6定义类的方式创建组件，好处是ES6可以有更多的操作 State & 生命周期
+class Clock extends React.Component {
+    //constructor 是一个类构造函数 固定格式
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+    render() {
+        return (
+            <div>
+                <h1>ES6封装的Clock类</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('clock')
+);
