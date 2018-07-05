1016a89e909f3482f13da55cc17ca4fefe8b801c
guanshibin <guanshibin@qing5.com>
2018-07-05 16:08:15 +0800

    * React 元素变量的用法 注意栗子里的 {button}

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -175,3 +175,60 @@ ReactDOM.render(
     <Greeting isLoggedIn={false} />,
     document.getElementById('greet')
 );
+
+//元素变量
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+    </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+    </button>
+    );
+}
+
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: false };
+    }
+
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+
+        let button = null;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} />;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('loginControl')
+);
