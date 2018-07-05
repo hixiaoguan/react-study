d38556bafda6b41df474b0d2f232790cbfbe763b
guanshibin <guanshibin@qing5.com>
2018-07-05 16:45:25 +0800

    * React 多组件渲染

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -299,3 +299,21 @@ ReactDOM.render(
     <Page />,
     document.getElementById('page')
 );
+//React 多组件渲染
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>
+            {number}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+    <NumberList numbers={numbers} />,
+    document.getElementById('moreCompoent')
+);
