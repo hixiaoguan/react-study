7f457b02b6d48ba7f4d61c3a727d3b9ed921d770
guanshibin <guanshibin@qing5.com>
2018-07-05 17:54:04 +0800

    * 状态提升（一）

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -364,3 +364,36 @@ ReactDOM.render(
     <NameForm />,
     document.getElementById('nameForm')
 );
+//状态提升
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>水会烧开</p>;
+    }
+    return <p>水不会烧开</p>;
+}
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { temperature: '' };
+    }
+
+    handleChange(e) {
+        this.setState({ temperature: e.target.value });
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>输入一个摄氏温度</legend>
+                <input value={temperature} onChange={this.handleChange} />
+                <BoilingVerdict celsius={parseFloat(temperature)} />
+            </fieldset>
+        );
+    }
+}
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('statusUp')
+);
