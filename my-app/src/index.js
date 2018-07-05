10c04833ef39ab598d5581be18d192ea1135685e
guanshibin <guanshibin@qing5.com>
2018-07-05 17:25:57 +0800

    * React 表单

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -317,3 +317,50 @@ ReactDOM.render(
     <NumberList numbers={numbers} />,
     document.getElementById('moreCompoent')
 );
+
+//React 表单
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Pick your favorite La Croix flavor:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+ReactDOM.render(
+    <NameForm />,
+    document.getElementById('nameForm')
+);
